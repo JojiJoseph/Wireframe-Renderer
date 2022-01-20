b821cf377b984c4df4f427dadd207f57e60c64af
joji <jojinattar@gmail.com>
2022-01-20 12:04:18 +0530

    Add object parser

---
+++ obj_parser.py
@@ -0,0 +1,24 @@
+
+def parse_obj(filename):
+    f = open("monkey.obj")
+    lines = f.readlines()
+    vertices = []
+    objects = []
+
+    for line in lines:
+        words = line.split()
+        if words[0] == "v":
+            vertices.append(list(map(float, words[1:])))
+        if words[0] == "f":
+            obj = []
+            for vertex_index in words[1:]:
+                idx = int(vertex_index.split("/")[0]) - 1
+                obj.append(
+                    list(vertices[idx])
+                )
+            objects.append(obj)
+
+    for object in objects:
+        for point in object:
+            point.append(1)
+    return objects
