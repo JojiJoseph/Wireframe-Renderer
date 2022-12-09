7cf184e9a3fbc7bb814653609838f182235fc55d
Joji Joseph <jojijoseph@iisc.ac.in>
2022-12-09 21:05:39 +0530

    refactor

--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
 import numpy as np
 
 def get_rotation_matrix(roll, pitch, yaw):
-    R1 = np.array([
+    roll_matrix = np.array([
         [1, 0, 0],
         [0, np.cos(roll), -np.sin(roll)],
         [0, np.sin(roll), np.cos(roll)],
         ])
-    R2 = np.array([
+    pitch_matrix = np.array([
         [np.cos(pitch), 0, np.sin(pitch)],
         [0, 1, 0],
         [-np.sin(pitch), 0, np.cos(pitch)],
         ])
-    R3 = np.array([
+    yaw_matrix = np.array([
         [np.cos(yaw), -np.sin(yaw), 0],
         [np.sin(yaw), np.cos(yaw), 0],
         [0, 0, 1],
         ])
-    return R3 @ R2 @ R1
+    return yaw_matrix @ pitch_matrix @ roll_matrix
